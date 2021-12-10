79add69d2e6bf5a89fd786a0393eb0d9c64ac626
Yasir <mdxyasir@gmail.com>
2021-12-10 19:23:50 +0000

    ban cmd

---
+++ commands/ban.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('(Staff) Bans a user from the server')
+        
+        .addUserOption(option => option.setName('member')
+            .setDescription('The user to ban')
+            .setRequired(true)
+        )
+        
+        .addStringOption(option => option.setName('reason')
+            .setDescription('The reason for banning the user')
+            .setRequired(false)
+        ),
+
+    role: 'Staff',
+    async execute(interaction) {
+
+        const user = interaction.options.getUser('member');
+        const reason = interaction.options.get('reason');
+        const member = await interaction.guild.members.cache.get(user.id) || await interaction.guild.members.fetch(user.id);
+
+        if (!member.bannable) { return interaction.reply({ content: 'I cannot ban this member', ephemeral: true }) }
+        
+        try {
+            await user.send({ content: `You have been banned from ${interaction.guild.name} for the following reason: ${reason}` });
+        } catch (error) { }
+
+        const embed = new MessageEmbed()
+            .setTitle('Member Banned')
+            .setDescription(`**${member.user.tag}** has been banned from the server`)
+
+        await interaction.reply({ embeds: [embed] });
+
+        member.ban({ reason: reason });
+    
+    }
+}

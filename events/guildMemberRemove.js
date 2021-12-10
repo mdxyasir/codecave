1ff2d1716878b43c421660da9cca8987ea8197e1
Yasir <mdxyasir@gmail.com>
2021-12-10 15:42:37 +0000

    leave message

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+const { updateMembers } = require("../functions/updateMembers")
+
+module.exports = {
+    name: 'guildMemberRemove',
+    execute(member) {
+        
+        // Leave Message
+        var channel = member.guild.channels.cache.get('918244467499229275');
+
+        const embed = new MessageEmbed()
+            .setColor('#db4335')
+            .setAuthor("Code Cave", member.guild.iconURL())
+            .setDescription(`**${member.user.username}#${member.user.discriminator}** has left the server`)
+
+        channel.send({
+            embeds: [embed]
+        })
+
+        // Update Member Count
+        updateMembers(member.guild)
+    
+    }
+}

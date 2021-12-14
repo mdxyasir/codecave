5e78da8f1166b2926b0c232dc2e026b7a6da0730
Yasir <mdxyasir@gmail.com>
2021-12-14 21:37:52 +0000

    role member view cmd

---
+++ commands/members.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("members")
+        .setDescription("View the members of a role")
+
+        .addStringOption(option => option.setName('role')
+            .setDescription('The role to view')
+            .setRequired(true)
+        ),
+
+    role: 'Staff',
+    async execute(interaction) {
+            
+            const roleName = interaction.options.getString('role');
+            const role = await interaction.guild.roles.cache.find(role => role.name === roleName) || await interaction.guild.roles.fetch(roleName);
+    
+            const embed = new MessageEmbed()
+                .setTitle(`Members of ${role.name}`)
+                .setDescription(`${role.members.map(member => `${member.user.tag}`).join('\n')}`)
+    
+            await interaction.reply({ embeds: [embed] });
+    
+        }
+}

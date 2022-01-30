37058b11c6dd389d13f7cfbdcab276e0179eef94
Yasir <mdxyasir@gmail.com>
2022-01-30 16:08:16 +0000

    staff help menu

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,13 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed, CommandInteraction } = require('discord.js');
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js') && file !== 'help.js');
+const staffFiles = fs.readdirSync('./commands/staff').filter(file => file.endsWith('.js'));
 
 const commandList = []
+const staffList = []
 
 for (const file of commandFiles) {
     const command = require(`./${file}`);
-
-    if (command.role == "Staff") { continue }
 
     const commandData = {
         name: "/" + command.data.name,
@@ -17,23 +17,58 @@ for (const file of commandFiles) {
     }
     
     commandList.push(commandData);
+}
 
+for (const file of staffFiles) {
+    const command = require(`./staff/${file}`);
+
+    const commandData = {
+        name: "/" + command.data.name,
+        description: command.data.description
+    }
+    
+    staffList.push(commandData);
 }
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription('Help menu'),
+		.setDescription('Help menu')
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('commands')
+                .setDescription('Regular help menu'))
+
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('staff')
+                .setDescription('Staff help menu')),
+
 	async execute(interaction) {
+
+        if (interaction.options.getSubcommand() === 'staff') {
+
+            const embed = new MessageEmbed()
+                .setTitle('Staff Help Menu')
+
+            for (const cmd of staffList) {
+                embed.addField(cmd.name, cmd.description);
+            }
+
+            await interaction.reply({ embeds: [embed] });
         
-        const embed = new MessageEmbed()
-            .setTitle('Code Cave Help Menu')
+        } else {
+        
+            const embed = new MessageEmbed()
+                .setTitle('Code Cave Help Menu')
 
-        for (const cmd of commandList) {
-            embed.addField(cmd.name, cmd.description);
-        }
+            for (const cmd of commandList) {
+                embed.addField(cmd.name, cmd.description);
+            }
 
-        await interaction.reply({ embeds: [embed] });
-            
+            await interaction.reply({ embeds: [embed] });
+
+        }   
 	},
 };

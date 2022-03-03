03da0ee1a845797dbbeb5ad8f5dc768d126080cc
Yasir <mdxyasir@gmail.com>
2022-03-03 18:25:10 +0000

    avatar cmd

---
+++ commands/avatar.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Get information about a user')
+
+        .addUserOption(option => option.setName('member')
+            .setDescription('The user to get information about')
+            .setRequired(true)
+        ),
+
+    async execute(interaction) {
+
+        const user = interaction.options.getUser('member');
+        const member = await interaction.guild.members.cache.get(user.id) || await interaction.guild.members.fetch(user.id);
+
+        const embed = new MessageEmbed()
+            .setTitle(`${member.user.username}'s Avatar`)
+            .setImage(member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }))
+            .setFooter({ text: `ID: ${member.user.id}`})
+
+        await interaction.reply({ embeds: [embed] });
+    
+    }
+}

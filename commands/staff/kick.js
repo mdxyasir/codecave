d40a03b52254e2d3141d1222f369c630a5ac37a0
Yasir <mdxyasir@gmail.com>
2022-05-23 20:53:02 +0100

    removed console.log()

--- commands/staff/kick.js
+++ commands/staff/kick.js
@@ -34,7 +34,6 @@ module.exports = {
             .setDescription(`**${member.user.tag}** has been kicked from the server`)
 
         await interaction.reply({ embeds: [embed] });
-        console.log(reason)
 
         member.kick(reason);
     

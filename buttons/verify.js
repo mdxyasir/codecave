0a1238f36c2dd4abd0289cad8b12d14419bb710a
Yasir <mdxyasir@gmail.com>
2022-10-30 13:54:50 +0000

    🩹 Member role ID

--- buttons/verify.js
+++ buttons/verify.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'verify',
 	async execute(interaction) {
 
-        var role = await interaction.member.guild.roles.cache.find(role => role.id === "");
+        var role = await interaction.member.guild.roles.cache.find(role => role.id === "1036248150081548299");
         await interaction.member.roles.add(role)
 
         await interaction.reply({ content: 'You have been verified!', ephemeral: true });
